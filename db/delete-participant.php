e9f65f713b5bdfe31e8ad141e6375a9ba9401318
Lalaux Clément <lalauxclement@gmail.com>
2022-06-01 17:01:24 +0200

    commit 3

---
+++ db/delete-participant.php
@@ -0,0 +1,14 @@
+<?php require('../templates/header.php');
+
+require('./db.php');
+
+
+global $conn;
+
+$conn->query('SET FOREIGN_KEY_CHECKS = 0');
+$conn->query('TRUNCATE TABLE tournoi_naruto');
+$conn->query('SET FOREIGN_KEY_CHECKS = 1');
+
+?>
+
+<?php require('../templates/footer.php'); ?>
---
+++ db/tournoi_naruto.php
@@ -0,0 +1,8 @@
+<?php 
+
+$tableau = [
+    ["Naruto Uzumaki",0,0,"5970514.jpg"],
+    []
+]
+
+?>
